6183fe45687bd45c7276c0b9f1f5235ea86936cb
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2019-11-07 07:41:42 +0900

    Qt Brynhildrを更新 (#8)

--- Casks/qt-brynhildr.rb
+++ Casks/qt-brynhildr.rb
@@ -1,6 +1,6 @@
 cask 'qt-brynhildr' do
-  version '2023'
-  sha256 'bf58070078f443d0e8ffb69bd1ab22981e9119232e9819891464e688f10d1e09'
+  version '2024'
+  sha256 'bba3bf216f5e2e11da9e935ff8d63594a313f0f6ea248ee1956d25663208aac5'
 
   url "https://github.com/funfun-dc5/qtbrynhildr/releases/download/v#{version}/QtBrynhildr-macosx_x64-v#{version}.dmg"
   appcast 'https://github.com/funfun-dc5/qtbrynhildr/releases.atom'
