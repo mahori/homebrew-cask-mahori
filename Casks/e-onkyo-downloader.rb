e7db4cb463dbae5b6d05acd70da93c01bc6fe588
mahori <gh@mahori.mail-box.ne.jp>
2019-11-07 07:54:32 +0900

    e-onkyo downloaderを追加

---
+++ Casks/e-onkyo-downloader.rb
@@ -0,0 +1,10 @@
+cask 'e-onkyo-downloader' do
+  version '1.18.1'
+  sha256 '6e7ab060067e9c19ba5e0b64dc0b9730b59748ed65f2e8ad97d2d70c6420390b'
+
+  url 'https://www.e-onkyo.com/downloader_osx/e-onkyoDownloader.pkg'
+  name 'e-onkyo downloader'
+  homepage 'https://www.codeweavers.com/products/crossover-mac/'
+
+  pkg 'e-onkyoDownloader.pkg'
+end
