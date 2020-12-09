d694f9a1c4fc9c9ea58c5634759f289aa9d24ea1
mahori <4198737+mahori@users.noreply.github.com>
2020-12-09 10:41:24 +0900

    PS Remote Playに名称を変更

--- Casks/playstation-remote-play.rb
+++ Casks/playstation-remote-play.rb
@@ -3,7 +3,7 @@ cask 'playstation-remote-play' do
   sha256 '8e77323c7de1d556c60938fcffe172ce1d4761b94c46f08404e68052c0141931'
 
   url 'https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg'
-  name 'PS4 Remote Play'
+  name 'PS Remote Play'
   homepage 'https://remoteplay.dl.playstation.net/remoteplay/lang/jp/'
 
   pkg 'RemotePlayInstaller.pkg'
