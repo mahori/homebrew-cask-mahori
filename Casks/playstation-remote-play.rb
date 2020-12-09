b569c00f03151aa9df92f07b8c03a5a20432f250
mahori <4198737+mahori@users.noreply.github.com>
2020-12-09 10:41:44 +0900

    PS Remote Playを更新

--- Casks/playstation-remote-play.rb
+++ Casks/playstation-remote-play.rb
@@ -1,6 +1,6 @@
 cask 'playstation-remote-play' do
   version '3.0.0'
-  sha256 '8e77323c7de1d556c60938fcffe172ce1d4761b94c46f08404e68052c0141931'
+  sha256 '595e8324cb850b5b9a8e63918e69ec655a24ee7884bfde9cd8eea448f257be23'
 
   url 'https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg'
   name 'PS Remote Play'
